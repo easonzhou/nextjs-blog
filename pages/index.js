c5d32574706bcc61f191f37320b16d0b2a07be99
Zhou, Yinsheng <yinsheng.zhou@lazada.com>
2021-07-05 09:36:48 +0800

    change

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>The author is too lazy to write anything</p>
+        <p>The author is awesome</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
